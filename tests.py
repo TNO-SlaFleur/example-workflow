af2e58535b45a5144c9a97b9ad63defc87b28ec9
Sebastiaan la Fleur <sebastiaan.lafleur@tno.nl>
2023-05-25 17:28:20 +0200

    Fail only on 3.10

--- tests.py
+++ tests.py
@@ -7,5 +7,7 @@ class TestFoor(TestCase):
 
         self.assertEqual('Hello', 'Hello')
 
-    def test__fails(self):
-        raise RuntimeError('This should fail')
+    def test__fails_on_3_10(self):
+        import sys
+        if sys.version_info[0] == 3 and sys.version_info[1] == 10:
+            raise RuntimeError('This should fail only on 3.10')
